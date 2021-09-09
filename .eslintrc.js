e1944f77d7a5f2777553b403dce88f50818a5b36
ccjmne <ccjmne@gmail.com>
2021-09-09 17:38:18 +0200

    Tweak and thematically organise .eslintrc.js conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,27 +24,54 @@ module.exports = {
     },
   },
   rules: {
-    'class-methods-use-this': ['error', { exceptMethods: ['connectedCallback', 'disconnectedCallback'] }],
-    'no-empty-function': 'off',
-    '@typescript-eslint/no-empty-function': ['error'],
-    'arrow-parens': ['error', 'as-needed'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['webpack.config.ts'] }],
-    'quotes': ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
-    'indent': ['error', 2],
-    'import/extensions': ['error', 'never'],
-    'semi': ['error', 'always'],
-    'max-len': ['warn', 140, { ignoreTrailingComments: true, ignorePattern: '^import .*' }],
-    'func-names': ['warn', 'as-needed'],
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error', { allow: ['_'] }],
-    'linebreak-style': ['error', 'unix'],
-    'quote-props': ['error', 'consistent-as-needed'],
-    'object-curly-newline': ['error', {
-      ImportDeclaration: 'never',
-      ObjectExpression: { consistent: true, multiline: true },
-      ObjectPattern: { consistent: true, multiline: true },
-    }],
-    'global-require': ['off'], // deprecated, see https://eslint.org/docs/rules/global-require
+    ...{
+      // ts overrides
+      'no-empty-function': 'off',
+      '@typescript-eslint/no-empty-function': ['error'],
+      'no-useless-constructor': 'off',
+      '@typescript-eslint/no-useless-constructor': ['error'],
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': ['error', { allow: ['_'] }],
+    },
+    ...{
+      // newlines
+      'linebreak-style': ['error', 'unix'],
+      'lines-between-class-members': ['warn', 'always', { exceptAfterSingleLine: true }],
+      'padded-blocks': ['error', { blocks: 'never', switches: 'never', classes: 'always' }],
+      'object-curly-newline': ['error', {
+        ImportDeclaration: 'never',
+        ObjectExpression: { consistent: true, multiline: true },
+        ObjectPattern: { consistent: true, multiline: true },
+      }],
+    },
+    ...{
+      // code style
+      'arrow-parens': ['error', 'as-needed'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'import/extensions': ['error', 'never'],
+      'import/order': ['error', {
+        'groups': ['builtin', 'external', 'internal', 'unknown', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always-and-inside-groups',
+        'alphabetize': { order: 'asc' },
+      }],
+      'indent': ['error', 2],
+      'max-len': ['error', 140, 4, {
+        ignoreTrailingComments: true,
+        ignorePattern: '^import\\s',
+        ignoreUrls: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      }],
+      'quotes': ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
+      'quote-props': ['error', 'consistent-as-needed'],
+      'semi': ['error', 'always'],
+    },
+    ...{
+      // code smells
+      'class-methods-use-this': ['error', { exceptMethods: ['connectedCallback', 'disconnectedCallback'] }],
+      'func-names': ['warn', 'as-needed'],
+      'global-require': ['off'], // deprecated, see https://eslint.org/docs/rules/global-require
+      'import/no-extraneous-dependencies': ['error', { devDependencies: ['webpack.config.ts'] }],
+    },
   },
 };
