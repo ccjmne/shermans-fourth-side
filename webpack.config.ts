95eb188ce1b278a77090c7d62e8b5c3d28d6fa2a
ccjmne <ccjmne@gmail.com>
2021-09-14 19:13:12 +0200

    Fix interoperation of webpack and typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,12 @@
+import { resolve } from 'path';
+
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin, { MinifyOptions } from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 // import svgToMiniDataURI from 'mini-svg-data-uri';
-import { resolve } from 'path';
-import { Configuration, EntryObject } from 'webpack';
+import { Configuration, EntryObject, WebpackPluginInstance } from 'webpack';
 import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
 import { author, description, keywords, name } from './package.json';
 
 const HTML_MINIFY_OPTS: MinifyOptions = {
@@ -35,58 +37,56 @@ export default (
     path: dist,
   },
   module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }, {
-        test: /exported-vars\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { modules: { compileType: 'icss' } },
+    rules: [{
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: ['ts-loader'],
+    }, {
+      test: /exported-vars\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { modules: { compileType: 'icss' } },
+        },
+        'sass-loader',
+      ],
+    }, {
+      test: /(?<!exported-vars)\.scss?$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      exclude: /node_modules/,
+    }, {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          esModule: false,
+          sources: {
+            list: [
+              { tag: 'img', attribute: 'src', type: 'src' },
+              { tag: 'img', attribute: 'data-src', type: 'src' },
+              { tag: 'video', attribute: 'src', type: 'src' },
+              { tag: 'video', attribute: 'data-src', type: 'src' },
+            ],
           },
-          'sass-loader',
-        ],
-      }, {
-        test: /(?<!exported-vars)\.scss?$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /node_modules/,
-      }, {
-        test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-          options: {
-            esModule: false,
-            sources: {
-              list: [
-                { tag: 'img', attribute: 'src', type: 'src' },
-                { tag: 'img', attribute: 'data-src', type: 'src' },
-                { tag: 'video', attribute: 'src', type: 'src' },
-                { tag: 'video', attribute: 'data-src', type: 'src' },
-              ],
-            },
-            minimize: mode === 'production' && HTML_MINIFY_OPTS,
-          },
-        }],
-      },
-    ],
-    // }, {
-    //   test: /\.svg$/,
-    //   // see https://webpack.js.org/guides/asset-modules/
-    //   type: 'asset/inline',
-    //   generator: { dataUrl: (content: unknown) => svgToMiniDataURI(content.toString()) },
-    // } as unknown], // TODO: remove 'as unknown' as soon as typings are updated for `RuleSetRule`
+          minimize: mode === 'production' && HTML_MINIFY_OPTS,
+        },
+      }],
+      // }, {
+      //   test: /\.svg$/,
+      //   // see https://webpack.js.org/guides/asset-modules/
+      //   type: 'asset/inline',
+      //   generator: { dataUrl: (content: unknown) => svgToMiniDataURI(content.toString()) },
+      // } as unknown], // TODO: remove 'as unknown' as soon as typings are updated for `RuleSetRule`
+    }],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '...'],
     // IMPORTANT: prioritise "global" node modules, for extending modules (e.g.: d3-selection-multi enhances d3-selection)
-    modules: [resolve(__dirname, 'node_modules'), 'node_modules'],
+    modules: [src, resolve(__dirname, 'node_modules'), 'node_modules'],
     mainFields: ['webpack', 'module', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
   },
-  plugins: [].concat(
+  plugins: ([] as WebpackPluginInstance[]).concat(
     new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' }),
     mode === 'production' ? new CleanWebpackPlugin() : [],
     new HtmlWebpackPlugin({
